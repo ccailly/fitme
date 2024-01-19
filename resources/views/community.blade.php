b404ee6a236856e28c58f610d9e6475b1f330a21
ArthurDsmt <119744690+ArthurCoding@users.noreply.github.com>
2024-01-19 12:23:46 +0000

    ajout du visuel d'une communauté

--- resources/views/community.blade.php
+++ resources/views/community.blade.php
@@ -1,3 +1,23 @@
 <x-app-layout title="Communauté">
+    <x-slot name="title">CommunautéName</x-slot>
+    <div class="container mx-auto mt-auto">
+        <div class="bg-white p-4 rounded shadow-md">
+            <div class="flex flex-row justify-between">
+                <div class="avatar placeholder">
+                    <div class="avatar">
+                        <div class="w-8 rounded-full">
+                            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
+                    </div>
+                </div>
+                <p class="text-2xl font-semibold">CommunityName</p>
+                <button class="btn btn-secondary">Rejoindre</button>
+            </div>
+            <div class="py-1">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis deserunt obcaecati mole</p>
+            </div>
+                <p class="text-end text-right py-0">XX suivent</p>
+        </div>
+    </div>
 </x-app-layout>
 
